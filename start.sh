a23ec137daf9cec181c5a05148780a9daeec24a1
sirrkitt <33910211+sirrkitt@users.noreply.github.com>
2020-02-17 11:46:01 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,4 @@
 
 wget https://papermc.io/api/v1/paper/1.15.2/latest/download -O /opt/papermc/paperclip.jar
 
-cd /opt/papermc/server
-
 exec java -server -Xms${RAM} -Xmx${RAM} -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+DisableExplicitGC -jar /opt/papermc/paperclip.jar nogui
